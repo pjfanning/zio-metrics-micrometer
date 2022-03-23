3a7d3ae785208660eb0e687afb99cb054cca72a1
PJ Fanning <pjfanning@users.noreply.github.com>
2022-03-23 21:43:56 +0100

    prep merge

--- src/main/scala/com/github/pjfanning/zio/micrometer/safe/Metric.scala
+++ src/main/scala/com/github/pjfanning/zio/micrometer/safe/Metric.scala
@@ -3,7 +3,6 @@ package com.github.pjfanning.zio.micrometer.safe
 import com.github.pjfanning.zio.micrometer.Counter
 import com.github.pjfanning.zio.micrometer.unsafe.{AtomicDouble, Registry, Counter => UnsafeCounter}
 import zio.{UIO, URIO}
-import zio.logging._
 
 import scala.util.control.NonFatal
 
@@ -17,7 +16,7 @@ object Counter extends LabelledMetric[Registry with Logging, Counter] {
     UnsafeCounter.labelled(name, help, labelNames).catchAll {
       case NonFatal(t) =>
         val logZio = log.throwable("Issue creating counter", t)
-        val fallbackZio = URIO.effectTotal {
+        val fallbackZio = URIO.succeed {
           (labelValues: Seq[String]) => {
             val atomicDouble = new AtomicDouble()
             new Counter {
@@ -37,7 +36,7 @@ object Counter extends LabelledMetric[Registry with Logging, Counter] {
     UnsafeCounter.unlabelled(name, help).catchAll {
       case NonFatal(t) =>
         val logZio = log.throwable("Issue creating counter", t)
-        val fallbackZio = URIO.effectTotal {
+        val fallbackZio = URIO.succeed {
           val atomicDouble = new AtomicDouble()
           new Counter {
             def inc(amount: Double): UIO[Unit] = URIO.succeed(atomicDouble.addAndGet(amount))
