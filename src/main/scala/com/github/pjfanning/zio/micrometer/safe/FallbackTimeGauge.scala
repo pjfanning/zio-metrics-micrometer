3f31c5a1a0e090767d97273c567f2c6ba558cafe
PJ Fanning <pjfanning@users.noreply.github.com>
2022-03-27 23:02:31 +0200

    Update FallbackTimeGauge.scala

--- src/main/scala/com/github/pjfanning/zio/micrometer/safe/FallbackTimeGauge.scala
+++ src/main/scala/com/github/pjfanning/zio/micrometer/safe/FallbackTimeGauge.scala
@@ -8,18 +8,18 @@ import zio.{Duration, UIO}
 import scala.compat.java8.DurationConverters.toScala
 import scala.concurrent.duration.{FiniteDuration, TimeUnit}
 
-private[safe] class FallbackTimeGauge(baseTimeUnit: TimeUnit) extends TimeGauge {
+private[safe] class FallbackTimeGauge(baseUnit: TimeUnit) extends TimeGauge {
   private val atomicDouble = new AtomicDouble()
-  override def baseTimeUnit: UIO[TimeUnit] = UIO.succeed(baseTimeUnit)
+  override def baseTimeUnit: UIO[TimeUnit] = UIO.succeed(baseUnit)
   override def totalTime(timeUnit: TimeUnit): UIO[Double] = UIO.succeed {
-    TimeUtils.convert(atomicDouble.get(), baseTimeUnit, timeUnit)
+    TimeUtils.convert(atomicDouble.get(), baseUnit, timeUnit)
   }
   override def startTimerSample(): UIO[TimerSample] = UIO.succeed {
     new TimerSample {
-      val startTime = zio.Runtime.default.unsafeRun(zio.Clock.currentTime(baseTimeUnit))
+      val startTime = zio.Runtime.default.unsafeRun(zio.Clock.currentTime(baseUnit))
       override def stop(): UIO[Unit] = {
         val task = for {
-          endTime <- zio.Clock.currentTime(baseTimeUnit)
+          endTime <- zio.Clock.currentTime(baseUnit)
         } yield {
           atomicDouble.addAndGet(endTime - startTime)
           ()
@@ -29,10 +29,10 @@ private[safe] class FallbackTimeGauge(baseTimeUnit: TimeUnit) extends TimeGauge 
     }
   }
   override def record(duration: Duration): UIO[Unit] = UIO.succeed {
-    val convertedDuration = toScala(duration).toUnit(baseTimeUnit)
+    val convertedDuration = toScala(duration).toUnit(baseUnit)
     atomicDouble.addAndGet(convertedDuration)
   }
   override def record(duration: FiniteDuration): UIO[Unit] = UIO.succeed {
-    atomicDouble.addAndGet(duration.toUnit(baseTimeUnit))
+    atomicDouble.addAndGet(duration.toUnit(baseUnit))
   }
 }
