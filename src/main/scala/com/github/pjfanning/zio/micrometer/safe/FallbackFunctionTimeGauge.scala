02355a1d2316c5cd7635326b2a5f568b32806a61
PJ Fanning <pjfanning@users.noreply.github.com>
2022-04-03 15:26:10 +0200

    wrong order for time units

--- src/main/scala/com/github/pjfanning/zio/micrometer/safe/FallbackFunctionTimeGauge.scala
+++ src/main/scala/com/github/pjfanning/zio/micrometer/safe/FallbackFunctionTimeGauge.scala
@@ -8,6 +8,6 @@ import scala.concurrent.duration.{Duration, TimeUnit}
 private[safe] class FallbackFunctionTimeGauge(baseUnit: TimeUnit, fun: => Double) extends ReadOnlyTimeGauge {
   override def baseTimeUnit: UIO[TimeUnit] = URIO.succeed(baseUnit)
   override def totalTime(timeUnit: TimeUnit): UIO[Double] = URIO.succeed {
-    Duration(fun, timeUnit).toUnit(baseUnit)
+    Duration(fun, baseUnit).toUnit(timeUnit)
   }
 }
--- src/main/scala/com/github/pjfanning/zio/micrometer/safe/FallbackTFunctionTimeGauge.scala
+++ src/main/scala/com/github/pjfanning/zio/micrometer/safe/FallbackTFunctionTimeGauge.scala
@@ -8,6 +8,6 @@ import scala.concurrent.duration.{Duration, TimeUnit}
 private[safe] class FallbackTFunctionTimeGauge[T](baseUnit: TimeUnit, t: T, fun: T => Double) extends ReadOnlyTimeGauge {
   override def baseTimeUnit: UIO[TimeUnit] = URIO.succeed(baseUnit)
   override def totalTime(timeUnit: TimeUnit): UIO[Double] = URIO.succeed {
-    Duration(fun(t), timeUnit).toUnit(baseUnit)
+    Duration(fun(t), baseUnit).toUnit(timeUnit)
   }
 }
