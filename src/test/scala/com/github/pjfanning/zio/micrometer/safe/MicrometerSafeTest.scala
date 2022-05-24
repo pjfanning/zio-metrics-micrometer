d047bf1a14ffeebec0825d20a0a4ab2298f3146f
PJ Fanning <pjfanning@users.noreply.github.com>
2022-05-24 20:52:42 +0100

    Update MicrometerSafeTest.scala

--- src/test/scala/com/github/pjfanning/zio/micrometer/safe/MicrometerSafeTest.scala
+++ src/test/scala/com/github/pjfanning/zio/micrometer/safe/MicrometerSafeTest.scala
@@ -1,13 +1,16 @@
 package com.github.pjfanning.zio.micrometer.safe
 
-import com.github.pjfanning.zio.micrometer.{Counter, TimeGauge, Timer}
+import com.github.pjfanning.zio.micrometer.{Counter, Gauge, HasMicrometerMeterId, ReadOnlyGauge, TimeGauge, Timer}
+import io.micrometer.core.instrument.Meter
 import io.micrometer.prometheus.{PrometheusConfig, PrometheusMeterRegistry}
 import zio.{Duration, ZIO}
 import zio.test.TestAspect._
 import zio.test.Assertion.{equalTo, isGreaterThanEqualTo}
 import zio.test.{ZIOSpecDefault, assert}
 
+import java.util.concurrent.atomic.AtomicReference
 import scala.concurrent.duration.{DurationLong, MILLISECONDS, NANOSECONDS}
+import scala.util.Random
 
 object MicrometerSafeTest extends ZIOSpecDefault {
 
@@ -19,6 +22,32 @@ object MicrometerSafeTest extends ZIOSpecDefault {
     _ <- c(Seq("get", "users")).inc()
     _ <- c(Array("get", "users")).inc(2)
   } yield c(Seq("get", "users"))
+
+  val counterZipTestZIO: ZIO[Registry, Throwable, (Meter.Id, Meter.Id)] = for {
+    c1 <- Counter.labelled("simple_counter", None, Seq("method", "resource"))
+    c2 <- Counter.labelled("simple_counter", None, Array("method", "resource"))
+    id1 <- c1(Seq("get", "users")).asInstanceOf[HasMicrometerMeterId].getMeterId
+    id2 <- c2(Array("get", "users")).asInstanceOf[HasMicrometerMeterId].getMeterId
+  } yield (id1, id2)
+
+  val gaugeTestZIO: ZIO[Registry, Throwable, Gauge] = for {
+    g <- Gauge.labelled("simple_gauge", None, Array("method", "resource"))
+    _ <- g(Array("get", "users")).inc()
+    _ <- g(Array("get", "users")).inc(2)
+    _ <- g(Array("get", "users")).dec(0.5)
+  } yield g(Seq("get", "users"))
+
+  val functionGaugeHolder = new AtomicReference[Double](10.0)
+  val functionGaugeTestZIO: ZIO[Registry, Throwable, ReadOnlyGauge] = for {
+    g <- Gauge.labelledFunction("simple_gauge", None, Array("method", "resource"),
+      () => functionGaugeHolder.get())
+  } yield g(Seq("get", "users"))
+
+  val tFunctionGaugeHolder = new AtomicReference[Double](10.0)
+  val tFunctionGaugeTestZIO: ZIO[Registry, Throwable, ReadOnlyGauge] = for {
+    g <- Gauge.labelledTFunction[AtomicReference[Double]]("simple_gauge", None, Array("method", "resource"),
+      tFunctionGaugeHolder, _.get())
+  } yield g(Seq("get", "users"))
 
   val timeGaugeTestZIO: ZIO[Registry, Throwable, TimeGauge] = for {
     g <- TimeGauge.labelled("plain_time_gauge", None, Array("method", "resource"))
@@ -46,6 +75,42 @@ object MicrometerSafeTest extends ZIOSpecDefault {
           counter <- counterTestZIO
           counterValue <- counter.get
         } yield assert(counterValue)(equalTo(3.0))
+      },
+      test("counter ids match") {
+        for {
+          tuple <- counterZipTestZIO
+          (id1, id2) = tuple
+        } yield assert(id1)(equalTo(id2))
+      }
+    ),
+    suite("Gauge")(
+      test("gauge increases and decreases by `inc/dec` amount") {
+        for {
+          gauge <- gaugeTestZIO
+          gaugeValue <- gauge.get
+        } yield assert(gaugeValue)(equalTo(2.5))
+      },
+      test("gauge based on function works") {
+        for {
+          gauge <- functionGaugeTestZIO
+          gaugeValue <- gauge.get
+        } yield assert(gaugeValue)(equalTo(functionGaugeHolder.get()))
+        functionGaugeHolder.set(Random.nextDouble())
+        for {
+          gauge <- functionGaugeTestZIO
+          gaugeValue <- gauge.get
+        } yield assert(gaugeValue)(equalTo(functionGaugeHolder.get()))
+      },
+      test("gauge based on t function works") {
+        for {
+          gauge <- tFunctionGaugeTestZIO
+          gaugeValue <- gauge.get
+        } yield assert(gaugeValue)(equalTo(tFunctionGaugeHolder.get()))
+        tFunctionGaugeHolder.set(Random.nextDouble())
+        for {
+          gauge <- tFunctionGaugeTestZIO
+          gaugeValue <- gauge.get
+        } yield assert(gaugeValue)(equalTo(tFunctionGaugeHolder.get()))
       }
     ),
     suite("TimeGauge")(
